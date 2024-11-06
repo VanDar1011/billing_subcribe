776f2b174df3d1b403eb2b2f862392e7c01c68d7
VietHoang10 <hoangde489@gmail.com>
2024-11-06 08:42:08 +0700

    fix entity

--- src/main/java/com/example/biling_system/model/Bill.java
+++ src/main/java/com/example/biling_system/model/Bill.java
@@ -2,37 +2,47 @@ package com.example.biling_system.model;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.sql.Date;
 
 @Entity
-@Table(name="bills")
+@Table(name = "bills")
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Bill {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_bill")
     int id;
-    @Column(length = 10,name="bill_code",unique=true)
+    @Column(length = 10, name = "bill_code", unique = true)
     String billCode;
-    @Column(length = 20,name="customer_code")
-    String customerCode;
-    @Column(length = 10,name="subcibe_code")
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_customer", referencedColumnName = "id_customer")
+    Customer idCustomer;
+    @Column(length = 10, name = "subcibe_code")
     String subcibeCode;
-    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-    @JoinColumn(name = "usage_package_id",referencedColumnName = "id")
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "usage_package_id", referencedColumnName = "id_usagepackage")
     UsagePackage usagePackageCode;
-    @Column(name="establishtment_day")
+    @Column(name = "establishtment_day")
     Date establishtmentDay;
-    @Column(name="collect_day")
+    @Column(name = "collect_day")
     Date collectionDay;
-    @Column(name="total_amount")
-    Double  totalAmount;
+    @Column(name = "total_amount")
+    Double totalAmount;
     @Column(length = 20)
     String status;
 
-
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idUsagePackage", referencedColumnName = "id_usagepackage")
+    UsagePackage idUsagePackage;
 
 
 }
--- src/main/java/com/example/biling_system/model/UsagePackage.java
+++ src/main/java/com/example/biling_system/model/UsagePackage.java
@@ -2,6 +2,9 @@ package com.example.biling_system.model;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.sql.Date;
@@ -9,27 +12,32 @@ import java.sql.Date;
 @Entity
 @Table(name = "usage_packages")
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsagePackage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usagepackage")
     int id;
-    @Column(length = 10,name="usage_package_code",unique=true)
+    @Column(length = 10, name = "usage_package_code", unique = true)
     String usagePackageCode;
-    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-    @JoinColumn(name = "package_id",referencedColumnName = "id")
-    Package packageObjs;
-    @Column(name="start_day")
+    @Column(name = "start_day")
     Date startDay;
-    @Column(name="end_day")
+    @Column(name = "end_day")
     Date endDay;
-    @Column(length = 20,name="check_status")
+    @Column(length = 20, name = "check_status")
     String checkoutStatus;
+    @Column(length = 20, name = "Note")
     String note;
+
     @ManyToOne
-    @JoinColumn(name = "id_subcriber")
-    Subcriber subcriber;
+    @JoinColumn(name = "id_subcriber", referencedColumnName = "id_phonenumber")
+    Subcriber IDsubcriber;
 
-
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_package", referencedColumnName = "id_package")
+    Package idPkg;
 
 
 }
