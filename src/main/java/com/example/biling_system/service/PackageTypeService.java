0373b691fadb9f1851557b1699100c4ac01b475f
VietHoang10 <hoangde489@gmail.com>
2024-11-06 13:41:44 +0700

    add all

--- src/main/java/com/example/biling_system/service/PackageTypeService.java
+++ src/main/java/com/example/biling_system/service/PackageTypeService.java
@@ -1,27 +1,27 @@
-//package com.example.biling_system.service;
-//
-//
-//import com.example.biling_system.Repository.PackageTypeRepository;
-//import com.example.biling_system.dto.request.PackageTypeRequest;
-//import com.example.biling_system.mapper.PackageTypeMapper;
-//import com.example.biling_system.model.PackageType;
-//import lombok.AccessLevel;
-//import lombok.RequiredArgsConstructor;
-//import lombok.experimental.FieldDefaults;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-//public class PackageTypeService {
-//
-//    PackageTypeRepository packageTypeRepository;
-//    PackageTypeMapper packageTypeMapper;
-//
-//    public PackageType createPackageType(PackageTypeRequest request) {
-//        PackageType packageType = new PackageType();
-//        packageType = packageTypeMapper.toPackageType(request);
-//        return packageType;
-//    }
-//
-//}
+package com.example.biling_system.service;
+
+
+import com.example.biling_system.Repository.PackageTypeRepository;
+import com.example.biling_system.dto.request.PackageTypeRequest;
+import com.example.biling_system.mapper.PackageTypeMapper;
+import com.example.biling_system.model.PackageType;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class PackageTypeService {
+
+    PackageTypeRepository packageTypeRepository;
+    PackageTypeMapper packageTypeMapper;
+
+    public PackageType createPackageType(PackageTypeRequest request) {
+        PackageType packageType = new PackageType();
+        packageType = packageTypeMapper.toPackageType(request);
+        return packageType;
+    }
+
+}
