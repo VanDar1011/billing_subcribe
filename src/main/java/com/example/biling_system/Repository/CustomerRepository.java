173cb83f21dedf2898cb7b9553b2437165ef9b67
dat <datthao27042002@gmail.com>
2024-11-06 15:47:35 +0700

    search oce

--- src/main/java/com/example/biling_system/Repository/CustomerRepository.java
+++ src/main/java/com/example/biling_system/Repository/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.example.biling_system.Repository;
 
 import com.example.biling_system.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
