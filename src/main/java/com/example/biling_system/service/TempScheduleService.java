af55ea524d040b40d2c3a3d48d15f4e590a4d862
dat <datthao27042002@gmail.com>
2024-11-19 12:14:55 +0700

    add code

---
+++ src/main/java/com/example/biling_system/service/TempScheduleService.java
@@ -0,0 +1,16 @@
+package com.example.biling_system.service;
+
+import com.example.biling_system.Repository.TempScheduleRepository;
+import com.example.biling_system.model.TempSchedule;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class TempScheduleService {
+    private final TempScheduleRepository tempScheduleRepository;
+    public void createTempSchedule(TempSchedule tempSchedule) {
+        tempScheduleRepository.save(tempSchedule);
+    }
+}
--- src/main/java/com/example/biling_system/service/TransactionService.java
+++ src/main/java/com/example/biling_system/service/TransactionService.java
@@ -1,6 +1,7 @@
 package com.example.biling_system.service;
 
 import com.example.biling_system.Repository.BillRepository;
+import com.example.biling_system.Repository.TempScheduleRepository;
 import com.example.biling_system.Repository.TransactionRepository;
 import com.example.biling_system.dto.request.TransactionRequest;
 import com.example.biling_system.dto.response.TransactionResponse;
@@ -8,6 +9,7 @@ import com.example.biling_system.exception.AppException;
 import com.example.biling_system.exception.ErrorCode;
 import com.example.biling_system.mapper.TransactionMapper;
 import com.example.biling_system.model.Bill;
+import com.example.biling_system.model.TempSchedule;
 import com.example.biling_system.model.Transaction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,7 @@ public class TransactionService {
     private final BillRepository billRepository;
     private final TransactionRepository transactionRepository;
     private final TransactionMapper transactionMapper;
+    private final TempScheduleService tempScheduleService;
 
     public String genTransactionCode() {
         long currentMilliseconds = new Date().getTime();
@@ -34,12 +37,19 @@ public class TransactionService {
         if(bill == null) {
             throw new AppException(ErrorCode.BILL_NOT_FOUND);
         }
+
+        // create record in transaction
         Transaction transaction = new Transaction();
         transaction.setIdBill(bill);
         transaction.setTotalAmount(bill.getTotalAmount());
         Date date = new Date();
         transaction.setTransactionDate(new java.sql.Date(date.getTime()) );
         transaction.setTransactionCode(genTransactionCode());
+        // create record in temp_schedule_table
+        TempSchedule tempSchedule = new TempSchedule();
+        tempSchedule.setTransactionCode(transaction.getTransactionCode());
+        tempScheduleService.createTempSchedule(tempSchedule);
+        //
         transaction.setPaymentMethod(transactionRequest.getPaymentMethod());
         Transaction repositoryTransaction = transactionRepository.save(transaction);
         return transactionMapper.toResponse(repositoryTransaction);
